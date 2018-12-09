0da067e4ce753cd5aa0e755e757c9a62f4fd92ef
dontehightower <dontedhightower@gmail.com>
2018-12-08 22:00:51 -0600

    add custom map component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-import ReactMapGL from 'react-map-gl';
+import Map from './components/Map';
 import './App.css';
-import config from './config';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ReactMapGL
-          width={1900}
-          height={1080}
-          latitude={30.2598}
-          longitude={-97.724}
-          zoom={13}
-          onViewportChange={viewport => {
-            const { width, height, latitude, longitude, zoom } = viewport;
-          }}
-          mapboxApiAccessToken={config.MAPBOX_GL_JS_KEY}
-        />
+        <Map />
       </div>
     );
   }
